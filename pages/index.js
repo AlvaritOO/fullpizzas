4abbd4a1a5ed88e4b36bc1aabdc513799bb2b0dc
Alvaro <alvaro.e.medina.a@gmail.com>
2020-09-22 11:16:57 -0300

    Tercer deploy

--- pages/index.js
+++ pages/index.js
@@ -27,35 +27,41 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>La casa de la Super Goloza</p>
-        <button className={styles.buttom}>Envia un WhatsApp</button>
+        <a
+          className={styles.button}
+          href="https://wa.me/message/CTYZQI5JZWJTO1"
+        >
+          Envia un WhatsApp &rarr;
+        </a>
+
         <div className={styles.grid}>
           <a href="" className={styles.card}>
-            <img src={RancaguaBBQ} alt="Rancagua" className={styles.img} />
-            <h3>Rancagua BBQ &rarr;</h3>
+            <img src={RancaguaBBQ} alt="RancaguaBBQ" className={styles.img} />
+            <h3>Rancagua BBQ </h3>
             <p>Queso, orégano, pepperoni, pollo, tocino y salsa barbacoa.</p>
           </a>
 
           <a href="" className={styles.card}>
-            <img src={Americana} alt="Rancagua" className={styles.img} />
-            <h3>Americana &rarr;</h3>
+            <img src={Americana} alt="Americana" className={styles.img} />
+            <h3>Americana</h3>
             <p>Queso, orégano, tocino, pollo, salame y choricillo.</p>
           </a>
 
           <a href="" className={styles.card}>
-            <img src={CuatroCarnes} alt="Rancagua" className={styles.img} />
-            <h3>Cuatro Carnes &rarr;</h3>
+            <img src={CuatroCarnes} alt="CuatroCarnes" className={styles.img} />
+            <h3>Cuatro Carnes</h3>
             <p>Queso, orégano, pepperoni, lomo, salame y choricillo</p>
           </a>
 
           <a href="" className={styles.card}>
-            <img src={Hawaiana} alt="Rancagua" className={styles.img} />
-            <h3>Hawaiana &rarr;</h3>
+            <img src={Hawaiana} alt="Hawaiana" className={styles.img} />
+            <h3>Hawaiana</h3>
             <p>Extra queso mozzarella, gauda, orégano, piña y jamón.</p>
           </a>
 
           <a href="" className={styles.card}>
-            <img src={CuatroQueso} alt="Rancagua" className={styles.img} />
-            <h3>Cuatro Queso &rarr;</h3>
+            <img src={CuatroQueso} alt="CuatroQueso" className={styles.img} />
+            <h3>Cuatro Queso</h3>
             <p>
               Salsa de tomate, orégano, queso mozzarella, gauda, edam y queso
               azul.
@@ -63,26 +69,30 @@ export default function Home() {
           </a>
 
           <a href="" className={styles.card}>
-            <img src={Italiana} alt="Rancagua" className={styles.img} />
-            <h3>Italiana &rarr;</h3>
+            <img src={Italiana} alt="Italiana" className={styles.img} />
+            <h3>Italiana</h3>
             <p>Queso, orégano, aceitunas negras, champiñón y salame.</p>
           </a>
 
           <a href="" className={styles.card}>
-            <img src={Napolitana} alt="Rancagua" className={styles.img} />
-            <h3>Napolitana &rarr;</h3>
+            <img src={Napolitana} alt="Napolitana" className={styles.img} />
+            <h3>Napolitana</h3>
             <p>Extra queso mozzarella, gauda, orégano, tomate y jamón.</p>
           </a>
 
           <a href="" className={styles.card}>
-            <img src={SuperPepperoni} alt="Rancagua" className={styles.img} />
-            <h3>Super Pepperoni &rarr;</h3>
+            <img
+              src={SuperPepperoni}
+              alt="SuperPepperoni"
+              className={styles.img}
+            />
+            <h3>Super Pepperoni</h3>
             <p>Queso mozzarella, gauda, orégano y doble pepperoni.</p>
           </a>
 
           <a href="" className={styles.card}>
-            <img src={Vegetariana} alt="Rancagua" className={styles.img} />
-            <h3>Vegetariana &rarr;</h3>
+            <img src={Vegetariana} alt="Vegetariana" className={styles.img} />
+            <h3>Vegetariana</h3>
             <p>
               Queso, orégano, pimiento verde, tomate, aceitunas negras y
               champiñón.
@@ -90,8 +100,8 @@ export default function Home() {
           </a>
 
           <a href="" className={styles.card}>
-            <img src={CuatroCarnes} alt="Rancagua" className={styles.img} />
-            <h3>Española &rarr;</h3>
+            <img src={CuatroCarnes} alt="CuatroCarnes" className={styles.img} />
+            <h3>Española</h3>
             <p>
               Queso, orégano, pimiento verde, tomate, lomito de cerdo y
               choricillo
@@ -101,7 +111,7 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a className={styles.logo}>Beta Site Web by FullPizzas - Rancagua</a>
+        <a>Beta - Sitio web de FullPizzas - Rancagua</a>
       </footer>
     </div>
   );
